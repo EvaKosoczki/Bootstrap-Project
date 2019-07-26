90f9c8961d285d1a61e40ddb5af4fea7111a1f58
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-07-26 18:29:54 +0200

    modals and stuff

--- js/main2.js
+++ js/main2.js
@@ -1,8 +1,17 @@
 $(window).on('scroll', function scroller() {
-  let y = $(window).scrollTop();
-  if (y == 0) {
+  const y = $(window).scrollTop();
+  if (y === 0) {
     $('.navbar').css('background-color', 'transparent');
+    $('.SB--yellow').css('color', '#fdcc52');
+    $('#Cont,#Down,#Feat').css('color', '#FFF');
   } else {
     $('.navbar').css('background-color', '#FFF')
+    $('.SB--yellow,#Cont,#Down,#Feat').css('color', '#636363');
   }
-})
+});
+
+$('.socMed-bg').tooltip();
+
+$('#Privacy').on('shown.bs.modal');
+$('#Terms').on('shown.bs.modal');
+$('#FAQ').on('shown.bs.modal');
